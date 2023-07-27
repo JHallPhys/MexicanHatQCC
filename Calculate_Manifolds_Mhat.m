5e183bac9a845baa95d4cad6a4cade3884a8d53b
Joe <josephhall0993@gmail.com>
2023-07-27 16:36:16 +0100

    tidied up readability a bit

--- Calculate_Manifolds_Mhat.m
+++ Calculate_Manifolds_Mhat.m
@@ -11,27 +11,18 @@ delta=1;
 system_flag='MKS';
 time_flag='fwd';
 mu=0.001 % Parameter that measures distance to fp at t=0
-N0=1; % Number of points
 t_final=80;
-t_final_bwd=30
+t_final_bwd=30 % If this is too high unstable manifold will move out too far
 qmin=-8
 qmax=8
 pmin=-8
 pmax=8
-% qmin=-20
-% qmax=20
-% pmin=-20
-% pmax=20
-
-
 nfp=2
 %==========================================================================
 % Construct Fixed points
 %==========================================================================
 
 [q_fp,p_fp]=get_fixed_points_mexican_hat(omega,beta,delta,gamma,system_flag); 
-
-
 
 %==========================================================================
 % Construct the Jacobian
@@ -50,186 +41,97 @@ xlabel('q')
 ylabel('p')
 axis([qmin qmax pmin pmax])
 
-
-
-
 %==========================================================================
 % Set up the initial conditions
 %==========================================================================
 
 Eval=diag(Eval);
-[~,ind_growing]=max(Eval); % Index of growing eignvalues
+[~,ind]=sort(Eval,'descend'); % Index of growing eigenvalue
+Eval=Eval(ind);
+Evec=Evec(:,ind);
+
 
 u1=Evec(:,1);
-u1=u1./norm(u1); % Normnalised eigenvector in growing direction
+u1=u1./norm(u1); % Normalised eigenvector in growing direction (used for stable manifold)
 
 u2=Evec(:,2);
-u2=u2./norm(u2); % Normnalised eigenvector in growing direction
-
-
-
-q_0=linspace(0,1,N0);
-p_0=linspace(0,1,N0);
-
-
-% 
-% q_stable=q_fp+q_0*mu*u1(1);
-% p_stable=p_fp+p_0*mu*u1(2);
+u2=u2./norm(u2); % Normalised eigenvector in decaying direction (used for unstable manifold)
 
 %==========================================================================
-%==========================================================================
-% This is bad and needs replaced
-% Know shouldnt be doing thiz
-%==========================================================================
+% Stable manifold
 %==========================================================================
 
+q_0=1;
+p_0=1;
 
 % One side
-
-q_stable=q_fp-q_0*mu*u2(1);
-p_stable=p_fp-p_0*mu*u2(2);
-
-
-
-for j=1:length(q_stable)
-    j
-    % Counter for the number we are on
-
-        % 1) Initial conditions in phase space       
-        z(1)=q_stable(j);
-        z(2)=p_stable(j);
-        z(3)=1;
-        
-          % 2) Propagate Norm
-        
-   [t,z_ps] = ode89(@(t,z) dhat(t,z,omega,beta,delta,gamma,system_flag,time_flag) ,[0 t_final],z); % Integrate
-        
-    figure(1)
-    hold on
-    plot(z_ps(:,1),z_ps(:,2),'r-','Markersize',1)
-    xlabel('q')
-    ylabel('p')
-    axis([qmin qmax pmin pmax])
-
-    pause(1)
-       
-    
-end
-
-
-% Other side
-
-q_stable=q_fp+q_0*mu*u2(1);
-p_stable=p_fp+p_0*mu*u2(2);
-
-
-%==========================================================================
-% Okay work the manifold
-%==========================================================================
-
-
-for j=1:length(q_stable)
-    j
-    % Counter for the number we are on
-
-        % 1) Initial conditions in phase space       
-        z(1)=q_stable(j);
-        z(2)=p_stable(j);
-        z(3)=1;
-        
-          % 2) Propagate Norm
-        
-       [t,z_ps] = ode89(@(t,z) dhat(t,z,omega,beta,delta,gamma,system_flag,time_flag) ,[0 t_final],z); % Integrate
-        
-    figure(1)
-    hold on
-    l2=plot(z_ps(:,1),z_ps(:,2),'r-','Markersize',1)
-    xlabel('q')
-    ylabel('p')
-    axis([qmin qmax pmin pmax])
-
-    pause(1)
-       
-    
-end
-
-% return
-
-%==========================================================================
-%==========================================================================
-% Untable manifold
-%==========================================================================
-%==========================================================================
-
-time_flag='bwd';
-t_final=t_final_bwd
-% One side
-
-q_stable=q_fp+q_0*mu*u1(1);
-p_stable=p_fp+p_0*mu*u1(2);
-
-
-
-for j=1:length(q_stable)
-    j
-    % Counter for the number we are on
-
-        % 1) Initial conditions in phase space       
-        z(1)=q_stable(j);
-        z(2)=p_stable(j);
-        z(3)=1;
-        
-          % 2) Propagate Norm
-        
-      [t,z_ps] = ode89(@(t,z) dhat(t,z,omega,beta,delta,gamma,system_flag,time_flag) ,[0 t_final],z); % Integrate
-        
-        
-    figure(1)
-    hold on
-    plot(z_ps(:,1),z_ps(:,2),'b-','Markersize',1)
-    xlabel('q')
-    ylabel('p')
-    axis([qmin qmax pmin pmax])
-
-    pause(1)
-       
-    
-end
-
-
-% Other side
 
 q_stable=q_fp-q_0*mu*u1(1);
 p_stable=p_fp-p_0*mu*u1(2);
+z=[q_stable,p_stable,0];
+[t,z_ps] = ode89(@(t,z) dhat(t,z,omega,beta,delta,gamma,system_flag,time_flag) ,[0 t_final],z); % Integrate
+    
+figure(1)
+hold on
+plot(z_ps(:,1),z_ps(:,2),'r-','Markersize',1)
+xlabel('q')
+ylabel('p')
+axis([qmin qmax pmin pmax])
 
+% Other side
 
+q_stable=q_fp+q_0*mu*u1(1);
+p_stable=p_fp+p_0*mu*u1(2);
+z=[q_stable,p_stable,0];
+[~,z_ps] = ode89(@(t,z) dhat(t,z,omega,beta,delta,gamma,system_flag,time_flag) ,[0 t_final],z); % Integrate
+   
+figure(1)
+hold on
+l2=plot(z_ps(:,1),z_ps(:,2),'r-','Markersize',1);
+xlabel('q')
+ylabel('p')
+axis([qmin qmax pmin pmax])
 
-for j=1:length(q_stable)
-    j
-    % Counter for the number we are on
+%==========================================================================
+% Unstable manifold
+%==========================================================================
 
-        % 1) Initial conditions in phase space       
-        z(1)=q_stable(j);
-        z(2)=p_stable(j);
-        z(3)=1;
+time_flag='bwd'; % Time-reversed dynamics
+t_final=t_final_bwd;
+
+% One side
+q_unstable=q_fp+q_0*mu*u2(1); % Now we use the 
+p_unstable=p_fp+p_0*mu*u2(2);
+z=[q_unstable,p_unstable,0];
+[~,z_ps] = ode89(@(t,z) dhat(t,z,omega,beta,delta,gamma,system_flag,time_flag) ,[0 t_final],z); % Integrate
         
-          % 2) Propagate Norm
+ figure(1)
+hold on
+plot(z_ps(:,1),z_ps(:,2),'b-','Markersize',1)
+xlabel('q')
+ylabel('p')
+axis([qmin qmax pmin pmax])        
+
+% Otherside
+
+q_unstable=q_fp-q_0*mu*u2(1);
+p_unstable=p_fp-p_0*mu*u2(2);
+z=[q_unstable,p_unstable,0];
+[~,z_ps] = ode89(@(t,z) dhat(t,z,omega,beta,delta,gamma,system_flag,time_flag) ,[0 t_final],z); % Integrate
         
-   [t,z_ps] = ode89(@(t,z) dhat(t,z,omega,beta,delta,gamma,system_flag,time_flag) ,[0 t_final],z); % Integrate
         
-        
-    figure(1)
-    hold on
-    l3=plot(z_ps(:,1),z_ps(:,2),'b-','Markersize',1)
-    xlabel('q')
-    ylabel('p')
-    legend([l1 l2 l3],{'Fixed point','Stable manifold','Unstable manifold'})
-    axis([qmin qmax pmin pmax])
-    box on
-    pause(1)
+figure(1)
+hold on
+l3=plot(z_ps(:,1),z_ps(:,2),'b-','Markersize',1);
+xlabel('q')
+ylabel('p')
+legend([l1 l2 l3],{'Fixed point','Stable manifold','Unstable manifold'})
+axis([qmin qmax pmin pmax])
+box on
+pause(1)
        
     
-end
+
 
 
 
